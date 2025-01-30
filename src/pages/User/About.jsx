c9c023308bef3444eda1846b8f2fcd21b591208c
Nidha70 <fathimanidhapk@gmail.com>
2025-01-30 14:31:35 +0530

    updated content

--- src/pages/User/About.jsx
+++ src/pages/User/About.jsx
@@ -8,7 +8,9 @@ function About() {
     return (
         <>
             <PopUp />
-                <HeaderBanner title="About Us" description="Learn more about our journey as the best business consultant in Kerala and what drives us forward."classname='bgTwo' 
+                <HeaderBanner title="About Us" description="
+Learn more about our journey as the best business consultant in Kerala and what drives us forward.
+"classname='bgTwo' 
                 brochureMain={true} noButton={true} class123={'headerbannerCenterContentOther'}/>
                 <section >
                     <OurTeam />  
