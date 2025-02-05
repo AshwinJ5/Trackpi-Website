a3d76d49165e4c2612e27ae1f8f484b547b2ecc0
Abhishek Pavithran <abhishekpavi6@gmail.com>
2025-02-05 20:57:27 +0530

    jobfair reg update

--- src/components/JobFair/Header.jsx
+++ src/components/JobFair/Header.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import { Row, Col } from 'react-bootstrap';
-import logo from '../../images/trackpil.png'
-import { IoCallOutline } from "react-icons/io5";
-import { IoMailOutline } from "react-icons/io5";
-
+import logo from '../../images/trackpil.png';
+import { IoCallOutline, IoMailOutline } from "react-icons/io5";
 import "../../CSS/JobFair/header.css";
 
 function HeaderSection() {
     return (
-        <div className='mt-3 ms-5 '>
-            <Row className='mt-5'>
+        <div className='mt-3 px-3 px-md-5'>
+            <Row className='mt-5 align-items-center text-center text-md-start'>
                 <Col lg={6} >
-                    <h5>Join TRACKPI'S Online</h5>
-                    <h1 >JOB <span className="highlight">FAIR</span></h1>
-                    <h2>Start Your Registration Process</h2>
+                    <h5 className='fw-bold'>Join TRACKPI'S Online</h5>
+                    <h1 className='fw-bold display-1'>
+                        JOB <span className="highlight">FAIR</span>
+                    </h1>
+                    <h2 className='fw-bold'>Start Your Registration Process</h2>
                     <p className="description">
                         Sign up for an exclusive online job fair to recruit top
                         professionals that can advance your company's goals.
@@ -21,14 +21,14 @@ function HeaderSection() {
                         ready to help your business grow.
                     </p>
                 </Col>
-                <Col lg={3} className="text  text-lg-start">
+                <Col lg={6} className="d-flex flex-column align-items-center text-md-start mt-4 mt-md-0">
                     <img src={logo} alt="TrackPi Job Fair Logo" className='img-fluid logo-img' />
-                    <h6 className="fw-bold">Your Strategic Growth Partner</h6>
-                    <div className="d-flex flex-column flex-md-row align-items-center align-items-md-start gap-2">
-                        <p className="mb-1 d-flex align-items-center gap-1">
+                    <h6 className="fw-bold text-center mt-2">Your Strategic Growth Partner</h6>
+                    <div className="contact-info d-flex flex-column flex-md-row align-items-center gap-2">
+                        <p className="mb-1 d-flex align-items-center gap-2 fw-bold">
                             <IoCallOutline /> <span>+91 8078179646</span>
                         </p>
-                        <p className="mb-0 d-flex align-items-center gap-1">
+                        <p className="mb-0 d-flex align-items-center gap-2 fw-bold">
                             <IoMailOutline /> <span>operations@trackpi.in</span>
                         </p>
                     </div>
