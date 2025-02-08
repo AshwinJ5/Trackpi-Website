5c69c1221b667649cfd5f973241f3d3cfa09261e
Shalu V <78534733+Shalu99v@users.noreply.github.com>
2025-02-08 15:58:12 +0530

    updated creators page

--- src/pages/User/Creators.jsx
+++ src/pages/User/Creators.jsx
@@ -3,8 +3,7 @@ import CreatorCard from '../../components/User/teamCreatorsCard';
 import creator1 from '../../images/creator1.png';
 import creator2 from '../../images/creator2.png';
 import '../../CSS/User/Creators.css';
-import baseURL from '../../Api Services/baseURL';
-import ashwin from "../../images/aswinImg.png"
+import { SERVER_URL } from '../../Api Services/serverUrl';
 // Sample data
 const designers = [
   { name: 'Alifen Sunny', role: 'UI/UX Designer', image: creator1 },
@@ -13,35 +12,19 @@ const designers = [
 
 const developers = [
   { name: 'Shalu V', role: 'React Developer', image: creator2 },
-  { name: 'Aswin Joseph', role: 'React Developer', image: ashwin },
+  { name: 'Aswin Joseph', role: 'React Developer', image: `${SERVER_URL}/uploads/images/ashwin.jpeg` },
   { name: 'Nidha Fathima', role: 'React Developer', image: creator2 },
 ];
-
+console.log(developers,"developers")
 
 
 const Creators = () => {
-  const [images, setImages] = useState([]);
-  console.log(images, 'image_response');
 
-
-  useEffect(() => {
-    const fetchImage = async () => {
-      try {
-        const response = await baseURL.get('/api/images/getImages');
-        setImages(response.data.images);
-
-      } catch (e) {
-        console.error(e);
-      }
-    };
-    fetchImage();
-  }, []);
   return (
     <div className="w-full xl:mt-[150px] mt-[90px] mb-20 flex flex-col items-center">
       <h2 className=" text-[22px] md:text-[28px] lg:text-[62px] font-bold text-center text-[#FF9D00] mb-0 ">
         Creators
       </h2>
-
       {/* Designers Section */}
       <div className="px-12">
         <h3 className="text-[18px]  md:text-[24px] lg:text-[46px]  font-bold text-[#FFC100] text-center mt-4 sm:mt-4 md:mt-8 lg:mt-2 2xl:mt-8  sm:mb-2 md:mb-4 lg:mb-4 xl:mb-8 2xl:mb-10">
