0d69e36ce939deb734a4e76e496e52037986c953
sreerag bhaskar k <sreeragvk020@gmail.com>
2025-02-15 20:09:12 +0530

    fix(upload): increase axios timeout to 10 minutes for large file uploads

--- src/Api Services/baseURL.js
+++ src/Api Services/baseURL.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
   baseURL: `${import.meta.env.VITE_SERVER_URL}/`,
-  timeout: 3000,
+  timeout: 600000,
   headers: {
     Authorization: `Bearer ${localStorage.getItem("adminToken")}`,
   },
